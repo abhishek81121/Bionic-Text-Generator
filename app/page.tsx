6591fabcaafd090f4755b1311efbed219b9a7c84
Abhishek Kumar Singh <abhishek81121@gmail.com>
2024-06-09 20:23:56 +0530

    Added the modal to show unsupported filr format

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,27 @@ import { Divider } from "@nextui-org/divider";
 import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { Input } from "@nextui-org/input";
 import { Button, ButtonGroup } from "@nextui-org/button";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from "@nextui-org/modal";
 
 export default function Home() {
   const [file, setFile] = useState<File>();
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [isVisible, setVisible] = useState(false);
+
   const [fileContent, setFileContent] = useState("");
   const fileInputRef = useRef<HTMLInputElement | null>(null);
   const handleFileChange = async (e: ChangeEvent<HTMLInputElement>) => {
     try {
       if (e.target.files) {
         if (e.target.files[0].name.slice(-3) != "txt") {
+          setVisible(true);
           console.log("unsupported format");
         } else {
           setFile(e.target.files[0]);
@@ -29,6 +41,7 @@ export default function Home() {
   };
   const handleButtonClick = () => {
     // Programmatically trigger the file input click event
+
     if (fileInputRef.current) fileInputRef.current.click();
   };
   return (
@@ -47,14 +60,13 @@ export default function Home() {
         />
         <Divider
           orientation="horizontal"
-          className="mt-10 h-1"
+          className="mt-10 h-1 mx-auto"
           style={{ width: "80vw" }}
         ></Divider>
         <Button className="mt-10" onClick={handleButtonClick}>
           Upload File
         </Button>
-        <br></br>
-        {file ? `${file.name}` : ""}
+
         <input
           type="file"
           className="hidden"
@@ -62,8 +74,37 @@ export default function Home() {
           accept=".txt"
           ref={fileInputRef}
         ></input>
-      </div>
+        <br></br>
+        <br></br>
 
+        <span>{file ? `${file.name}` : ""}</span>
+        <Divider
+          orientation="horizontal"
+          className="mt-10 h-1 mx-auto"
+          style={{ width: "80vw" }}
+        ></Divider>
+      </div>
+      <Modal isOpen={isVisible} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Unsupported File Format
+              </ModalHeader>
+              <ModalBody>Only Text File Is Supported For Now</ModalBody>
+              <ModalFooter>
+                <Button
+                  color="danger"
+                  variant="light"
+                  onPress={() => setVisible(false)}
+                >
+                  Close
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
       <BackgroundBeams />
     </div>
   );
